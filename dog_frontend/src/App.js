175d2c3f70b3bd5d9eef66555e34ef2595f47e06
tiffanysimione <simionetiffany1@gmail.com>
2023-04-13 11:17:51 -0400

    create dog

--- dog_frontend/src/App.js
+++ dog_frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home'
 import Update from "./Update";
 import Read from "./Read";
+import CreateDog from './CreateDog';
 
 
 
@@ -14,6 +15,7 @@ function App(){
         <Route path ='/' element ={<Home />}></Route>
         <Route path ='/update/:id' element ={<Update />}></Route>
         <Route path ='/read/:id' element ={<Read />}></Route>
+        <Route path ='/CreateDog/' element ={<CreateDog />}></Route>
 
 
 
