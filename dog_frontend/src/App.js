b3f87f14598b157bae28b6b711b6920b6e5d4a11
tiffanysimione <simionetiffany1@gmail.com>
2023-04-13 12:34:17 -0400

    pushing single page

--- dog_frontend/src/App.js
+++ dog_frontend/src/App.js
@@ -1,41 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-
-import Home from './Home';
-import Update from './Update';
-import Read from './Read';
-import CreateDog from './CreateDog';
-
-function App() {
-  const [dogs, setDogs] = useState([]);
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Add from './components/Add.js'
+import Edit from './components/Edit.js'
+const App = () => {
+  let [dog, setDog] = useState([])
+  const handleDelete = (event) => {
+    axios
+      .delete('http://localhost:8000/api/dog/' + event.target.value)
+      .then((response) => {
+        getDog()
+      })
+  }
+  const handleCreate = (addDog) => {
+    axios
+      .post('http://localhost:8000/api/dog', addDog)
+      .then((response) => {
+        console.log(response)
+        getDog()
+      })
+  }
+  const getDog = () => {
+    axios
+      .get('http://localhost:8000/api/dog')
+      .then(
+        (response) => setDog(response.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error(error))
+  }
+  const handleUpdate = (editDog) => {
+    console.log(editDog)
+    axios
+      .put('http://localhost:8000/api/dog/' + editDog.id, editDog)
+      .then((response) => {
+        getDog()
+      })
+  }
   useEffect(() => {
-    const fetchDogs = async () => {
-      const response = await axios.get('/api/dogs');
-      setDogs(response.data);
-    };
-    fetchDogs();
-  }, []);
-
-  const handleCreateDog = async (dog) => {
-    const response = await axios.post('/api/dogs', dog);
-    setDogs([...dogs, response.data]);
-  };
-
+    getDog()
+  }, [])
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home dogs={dogs} />} />
-        <Route path="/update/:id" element={<Update />} />
-        <Route path="/read/:id" element={<Read />} />
-        <Route
-          path="/CreateDog/"
-          element={<CreateDog handleCreateDog={handleCreateDog} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>Doggy Dog World</h1>
+      <Add handleCreate={handleCreate} />
+      <div className="dogs">
+        {dog.map((dog) => {
+          return (
+            <div className="card" key={dog.id}>
+              <div className="card-header">
+                <h5>Name: {dog.name}</h5>
+                <h5>Breed: {dog.breed}</h5>
+                <h5>Weight: {dog.weight}</h5>
+                <h5>Birthdate: {dog.birthdate}</h5>
+              </div>
+              <div className="card-body">
+                <Edit handleUpdate={handleUpdate} dog={dog} />
+                <button onClick={handleDelete} value={dog.id}>
+                  X
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
-
 export default App;
---
+++ dog_frontend/src/components/Edit.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+const Edit = (props) => {
+    const [dog, setDog] = useState({...props.dog})
+  const handleChange = (event) => {
+    setDog({ ...dog, [event.target.name]: event.target.value })
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.handleUpdate(dog)
+  }
+  return (
+    <>
+      <details>
+        <summary>Edit Doggy Info</summary>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={dog.name}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label htmlFor="breed">Breed: </label>
+          <input
+            type="text"
+            name="breed"
+            value={dog.breed}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label htmlFor="weight">Weight: </label>
+          <input
+            type="number"
+            name="weight"
+            value={dog.model}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label htmlFor="birthdate">Birthdate: </label>
+          <input
+            type="date"
+            name="birthdate"
+            value={dog.birthdate}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input type="submit" />
+        </form>
+      </details>
+    </>
+  )
+}
+export default Edit
