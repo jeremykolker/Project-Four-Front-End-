eef582280090d0991c25f2ebe45eff26d872ca5c
“Jeremy <jeremy.kolker@gmail.com>
2023-04-16 13:10:30 -0400

    total revamp

--- dog_frontend/src/App.js
+++ dog_frontend/src/App.js
@@ -1,40 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import Add from './components/Add.js';
+import Edit from './components/Edit.js';
 
-import Home from './Home';
-import Update from './Update';
-import Read from './Read';
-import CreateDog from './CreateDog';
+const App = () => {
+  let [dog, setDog] = useState([]);
+  let [sortBy, setSortBy] = useState('');
+  let [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef();
 
-function App() {
-  const [dogs, setDogs] = useState([]);
-
-  useEffect(() => {
-    const fetchDogs = async () => {
-      const response = await axios.get('/api/dogs');
-      setDogs(response.data);
-    };
-    fetchDogs();
-  }, []);
-
-  const handleCreateDog = async (dog) => {
-    const response = await axios.post('/api/dogs', dog);
-    setDogs([...dogs, response.data]);
+  const handleDelete = (event) => {
+    axios.delete('http://localhost:8000/api/dog/' + event.target.value).then((response) => {
+      getDog();
+    });
   };
 
+  const handleCreate = (addItem) => {
+    axios.post('http://localhost:8000/api/dogs', addItem).then((response) => {
+      console.log(response);
+      getDog();
+    });
+  };
+
+  const getDog = () => {
+    axios
+      .get('http://localhost:8000/api/dogs')
+      .then((response) => {
+        setDog(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleUpdate = (editDog) => {
+    console.log(editDog);
+    axios.put('http://localhost:8000/api/dogs/' + editDog.id, editDog).then((response) => {
+      getDog();
+    });
+  };
+
+  useEffect(() => {
+    getDog();
+  }, []);
+
+  const sortByName = () => {
+    setSortBy('name');
+    setDog([...dog].sort((a, b) => (a.name > b.name ? 1 : -1)));
+    setMenuOpen(false);
+  };
+
+  const sortByWalkTime = () => {
+    setSortBy('walk_time');
+    setDog([...dog].sort((a, b) => (a.walk_time > b.walk_time ? 1 : -1)));
+    setMenuOpen(false);
+  };
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home dogs={dogs} />} />
-        <Route path="/update/:id" element={<Update />} />
-        <Route path="/read/:id" element={<Read />} />
-        <Route
-          path="/CreateDog/"
-          element={<CreateDog handleCreateDog={handleCreateDog} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>Doggy Dog World</h1>
+      <div className="menu-container" ref={menuRef}>
+        <div className="menu-header" onClick={handleMenuClick}>
+          <h2>≡</h2>
+          {menuOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}
+        </div>
+        {menuOpen && (
+          <div className="menu-body">
+            <button onClick={sortByName}>Name</button>
+            <button onClick={sortByWalkTime}>Walk Time</button>
+          </div>
+        )}
+      </div>
+      <div className="add">
+        <h2>Add Dog</h2>
+        <Add handleCreate={handleCreate} />
+      </div>
+      <div className="items">
+        {dog.map((dog) => {
+          return (
+            <div className="card" key={dog.id}>
+              <h5>Name: {dog.name}</h5>
+              <h5>Age: {dog.age}</h5>
+              <h5>Breed: {dog.breed}</h5>
+              <h5>Walk Time: {dog.walk_time}</h5>
+              <h5>Feeding Instructions: {dog.feeding_instructions}</h5>
+              <div className="edit">
+                <Edit handleUpdate={handleUpdate} dog={dog} />
+              </div>
+              <button onClick={handleDelete} value={dog.id}>
+                X
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
+
 export default App;
--- dog_frontend/src/components/Add.js
+++ dog_frontend/src/components/Add.js
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
 
 const Add = (props) => {
-  let emptyDog = { name: '', breed: '', weight: '', birthdate: '' }
-  const [dog, setDog] = useState(emptyDog)
+  const [dog, setDog] = useState({
+    name: '',
+    breed: '',
+    age: '',
+    walk_time: '',
+    feeding_instructions: ''
+  });
+
   const handleChange = (event) => {
-    setDog({ ...dog, [event.target.name]: event.target.value })
-  }
+    setDog({ ...dog, [event.target.name]: event.target.value });
+  };
+
   const handleSubmit = (event) => {
-    event.preventDefault()
-    props.handleCreate(dog)
-  }
+    event.preventDefault();
+    props.handleCreate(dog);
+    setDog({
+      name: '',
+      breed: '',
+      age: '',
+      walk_time: '',
+      feeding_instructions: ''
+    });
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
-        <input
-          type="text"
-          name="name"
-          value={dog.name}
-          onChange={handleChange}
-        />
+        <input type="text" name="name" value={dog.name} onChange={handleChange} />
         <br />
         <br />
         <label htmlFor="breed">Breed: </label>
+        <input type="text" name="breed" value={dog.breed} onChange={handleChange} />
+        <br />
+        <br />
+        <label htmlFor="age">Age: </label>
+        <input type="number" name="age" value={dog.age} onChange={handleChange} />
+        <br />
+        <br />
+        <label htmlFor="walk_time">Walk Time: </label>
+        <input type="time" name="walk_time" value={dog.walk_time} onChange={handleChange} />
+        <br />
+        <br />
+        <label htmlFor="feeding_instructions">Feeding Instructions: </label>
         <input
           type="text"
-          name="breed"
-          value={dog.breed}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <label htmlFor="weight">Weight: </label>
-        <input
-          type="number"
-          name="weight"
-          value={dog.weight}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <label htmlFor="birthdate">Birthdate: </label>
-        <input
-          type="date"
-          name="birthdate"
-          value={dog.birthdate}
+          name="feeding_instructions"
+          value={dog.feeding_instructions}
           onChange={handleChange}
         />
         <br />
@@ -52,6 +56,7 @@ const Add = (props) => {
         <input type="submit" />
       </form>
     </>
-  )
-}
-export default Add
+  );
+};
+
+export default Add;
